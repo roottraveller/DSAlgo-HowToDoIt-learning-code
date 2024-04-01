2f3bdebe294c5248bcde422051ef4e7a5196bb54
Rishi <rishi@MacBook-Pro.local>
2024-04-02 00:12:32 +0530

    2D array sorting example

--- java-code/src/main/java/TwoDArraysSort.java
+++ java-code/src/main/java/TwoDArraysSort.java
@@ -1,29 +1,59 @@
-import java.util.*;
-
-/* 
- * Author @ roottraveller, July 2017
- */
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class TwoDArraysSort {
     public static void main(String[] args) {
-        Integer[][] myarr = {{0, 10}, {10, 9}, {2, 9}, {3, 9}, {4, 15}, {5, 10}, {6, 4}};
+        testMixedSorting();
+        testIntegerSorting();
+    }
 
-        for (Integer[] elem : myarr) {
-            System.out.print(elem[0] + "," + elem[1]);
-            System.out.println();
-        }
+    private static void testMixedSorting() {
+        String[][] data = {
+                {"300", "user_1", "resource_3"},
+                {"599", "user_1", "resource_1"},
+                {"100", "user_1", "resource_3"},
+                {"1199", "user_1", "resource_2"},
+                {"1200", "user_1", "resource_3"},
+                {"1201", "user_1", "resource_2"},
+                {"1202", "user_1", "resource_1"}
+        };
 
-        Arrays.sort(myarr, new Comparator<Integer[]>() {
-            @Override
-            public int compare(Integer[] data1, Integer[] data2) {
-                return data1[1].compareTo(data2[1]);    // sort on column 2
-                // return data1[0].comapreTo(data2[0]); // sort on column 1
+        Arrays.sort(data, (a, b) -> {
+            if (a[2].compareTo(b[2]) == 0) {
+                return Integer.parseInt(a[0]) - Integer.parseInt(b[0]);
             }
+            return a[2].compareTo(b[2]);
         });
 
-        for (Integer[] elem : myarr) {
-            System.out.println(elem[0] + "," + elem[1]);
+        System.out.println("Sorted on resource then value");
+        for (String[] row : data) {
+            for (String cell : row) {
+                System.out.print(cell + " ");
+            }
             System.out.println();
         }
-    } // main() ends
+    }
+
+    private static void testIntegerSorting() {
+        Integer[][] data = {
+                {0, 10}, {10, 9}, {2, 9}, {3, 9}, {4, 15}, {5, 10}, {6, 4}
+        };
+
+        System.out.println("Unsorted array");
+        for (Integer[] elem : data) {
+            System.out.println("{" + elem[0] + "," + elem[1] + "}");
+        }
+
+        Arrays.sort(data, Comparator.comparing(arr -> arr[0]));
+        System.out.println("Sorted on first column");
+        for (Integer[] elem : data) {
+            System.out.println("{" + elem[0] + "," + elem[1] + "}");
+        }
+
+        Arrays.sort(data, Comparator.comparing(arr -> arr[1]));
+        System.out.println("Sorted on second column");
+        for (Integer[] elem : data) {
+            System.out.println("{" + elem[0] + "," + elem[1] + "}");
+        }
+    }
 }
