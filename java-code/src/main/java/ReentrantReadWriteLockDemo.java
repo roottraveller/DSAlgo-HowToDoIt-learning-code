92e544070ef897e4b140b8e660859c8f15272c36
roottraveller <roottraveller@users.noreply.github.com>
2024-04-23 00:58:35 +0530

    Update and rename ReentrantReadWriteLockDemo to ReentrantReadWriteLockDemo.java

--- java-code/src/main/java/ReentrantReadWriteLockDemo.java
+++ java-code/src/main/java/ReentrantReadWriteLockDemo.java
@@ -1,6 +1,33 @@
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/*
+ * Lock Class Hierarchy:
+ * 
+ *                 (I) java.util.concurrent.locks.Lock
+ *                          |
+ *            ______________|________________
+ *           |                                |
+ *       (C) ReentrantLock     (C) ReentrantReadWriteLock
+ *           |                                |
+ *           |                                |
+ *       (I) Condition                (C) ReadLock    (C) WriteLock
+ *                                              |
+ *                                              |
+ *                                        (C) Sync.Mutex
+ */
+
+/*
+ * Read Lock:
+ *     - Allows multiple threads to acquire the lock simultaneously for reading.
+ *     - While a thread holds a read lock, other threads can also acquire read locks, but no thread can acquire a write lock until all read locks are released.
+ *     - Read locks are shared, meaning multiple threads can hold them simultaneously.
+ *
+ * Write Lock:
+ *     - Grants exclusive access to a single thread for writing.
+ *     - While a thread holds a write lock, no other thread can acquire either a read or write lock until the write lock is released.
+ *     - Write locks are exclusive, meaning only one thread can hold the write lock at any given time.
+ */
 public class ReentrantReadWriteLockDemo {
     private static final int READERS_COUNT = 3;
     private static final int WRITERS_COUNT = 2;
