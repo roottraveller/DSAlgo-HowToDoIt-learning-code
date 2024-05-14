f0972dd74a7f027d16cb99417371fb9fbdf2d6cd
roottraveller <roottraveller@users.noreply.github.com>
2024-05-14 13:22:47 +0530

    Create QuickSort.java

---
+++ java-code/src/main/java/sorting/QuickSort.java
@@ -0,0 +1,77 @@
+import java.util.List;
+
+public class QuickSort {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                // Swap array[i] and array[j]
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        // Swap array[i+1] and array[high] (pivot)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+
+    public static void quickSort(List<String> list, int low, int high) {
+        if (low < high) {
+            int pi = partition(list, low, high);
+            quickSort(list, low, pi - 1);
+            quickSort(list, pi + 1, high);
+        }
+    }
+
+    public static int partition(List<String> list, int low, int high) {
+        String pivot = list.get(high);
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (list.get(j).compareTo(pivot) < 0) { // Or compareToIgnoreCase
+                i++;
+                // Swap list[i] and list[j]
+                String temp = list.get(i);
+                list.set(i, list.get(j));
+                list.set(j, temp);
+            }
+        }
+        // Swap list[i+1] and list[high] (pivot)
+        String temp = list.get(i + 1);
+        list.set(i + 1, list.get(high));
+        list.set(high, temp);
+        return i + 1;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        quickSort(array, 0, array.length - 1);
+        System.out.println("Sorted array:");
+        printArray(array);
+
+        List<String> list = List.of("banana", "apple", "orange", "grape", "pineapple");
+        quickSort(list, 0, list.size() - 1);
+        System.out.println("Sorted list:");
+        System.out.println(list);
+    }
+}
